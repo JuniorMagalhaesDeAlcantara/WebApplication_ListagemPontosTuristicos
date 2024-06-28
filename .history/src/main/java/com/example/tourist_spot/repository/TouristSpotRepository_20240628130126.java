2cb000fea205f6fdb0a35317825ea5a3854d6c69
Junior Alcantara <jrlevita09@hotmail.com>
2024-06-28 13:10:11 -0300

    Criação do Repositorio

---
+++ .history/src/main/java/com/example/tourist_spot/repository/TouristSpotRepository_20240628130126.java
@@ -0,0 +1,5 @@
+package com.example.tourist_spot.repository;
+
+public class TouristSpotRepository {
+
+}
---
+++ src/main/java/com/example/tourist_spot/repository/TouristSpotRepository.java
@@ -0,0 +1,19 @@
+package com.example.tourist_spot.repository;
+
+// Importa as classes necessárias do Spring Data
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.tourist_spot.model.TouristSpot;
+
+// Interface que estende JpaRepository para realizar operações de banco de dados com a entidade TouristSpot
+public interface TouristSpotRepository extends JpaRepository<TouristSpot, Long> {
+    
+    // Declaração de um método de consulta customizado para encontrar TouristSpot
+    // O método busca pontos turísticos cujo nome, descrição ou localização contenham o texto fornecido
+    // A busca é paginada, permitindo resultados em páginas
+    Page<TouristSpot> findByNameContainingOrDescriptionContainingOrLocationContaining(
+        String name, String description, String location, Pageable pageable);
+}
+
